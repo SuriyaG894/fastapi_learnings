fde0ab9b2724abcff796e0c4b9967188af24ed7c
suriyaganesh-tcm <suriyaganesh@tickingminds.com>
2025-12-28 14:05:38 +0530

    Handling Explict status code and raise HTTPException

--- books2.py
+++ books2.py
@@ -1,8 +1,9 @@
 
 from typing import Optional
 
-from fastapi import FastAPI,Path,Query
+from fastapi import FastAPI,Path,Query,HTTPException
 from pydantic import BaseModel,Field
+from starlette import status
 
 app = FastAPI()
 
@@ -55,11 +56,11 @@ BOOKS = [
     Book(6,"HP3","Author Three","Book Description",1,1997)
 ]
 
-@app.get("/books")
+@app.get("/books",status_code=status.HTTP_200_OK)
 def read_all_books():
     return BOOKS
 
-@app.post("/books")
+@app.post("/books",status_code=status.HTTP_201_CREATED)
 def add_new_book(book_request:BookRequest):
     new_book = Book(**book_request.model_dump())
     BOOKS.append(update_id(new_book))
@@ -68,7 +69,7 @@ def update_id(book:Book):
     book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id +1
     return book
 
-@app.get("/books/{book_id}")
+@app.get("/books/{book_id}",status_code=status.HTTP_200_OK)
 def fetch_book_by_id(book_id:int = Path(gt=0)):
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
@@ -76,16 +77,20 @@ def fetch_book_by_id(book_id:int = Path(gt=0)):
     return {"message":"Book not found"}
 
 
-@app.get("/books/")
+@app.get("/books/",status_code=status.HTTP_200_OK)
 def fetch_book_by_ratings(rating:int = Query(gt=0,lt=6)):
     books_by_rating =[]
+    flag = 0
     for i in range(len(BOOKS)):
         if BOOKS[i].rating == rating:
             books_by_rating.append(BOOKS[i])
-    return books_by_rating if len(books_by_rating)>0 else {"message":"Books not found"}
+            flag = 1
+    if flag == 0:
+        raise HTTPException(status_code=404,detail="Book not found")
+    return books_by_rating
 
 
-@app.put("/books/")
+@app.put("/books/",status_code=status.HTTP_204_NO_CONTENT)
 def update_book(upt_book:BookRequest):
     updated_book = Book(**upt_book.model_dump())
     flag = 0
@@ -93,26 +98,31 @@ def update_book(upt_book:BookRequest):
         if BOOKS[i].id == updated_book.id:
             BOOKS[i] = updated_book
             flag =1
-            break
     if flag == 0:
         updated_book = update_id(updated_book)
         BOOKS.append(updated_book)
-    return {"message":"Book updated successfully"}
+        raise HTTPException(status_code=404,detail="Book not found")
 
-@app.delete("/books/{book_id}")
+
+@app.delete("/books/{book_id}",status_code=status.HTTP_204_NO_CONTENT)
 def delete_book_by_id(book_id:int = Path(gt=0)):
+    flag = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
             BOOKS.pop(i)
-            return {"message":"Book deleted successfully"}
-    return {"message":"Book not found"}
+            flag =True
+            print("Hi ")
+    if not flag:
+        print("Falg",flag)
+        raise HTTPException(status_code=404,detail="Book not found")
 
 # Assignment to fetch book using published_date
-@app.get("/books/publish/")
-def fetch_book_by_published_date(published_date:int):
+@app.get("/books/publish/",status_code=status.HTTP_200_OK)
+def fetch_book_by_published_date(published_date:int = Query(gt=999,lt=2037)):
     books_by_published_date = []
     print(published_date)
     for i in range(len(BOOKS)):
         if BOOKS[i].published_date == published_date:
             books_by_published_date.append(BOOKS[i])
     return books_by_published_date
+
